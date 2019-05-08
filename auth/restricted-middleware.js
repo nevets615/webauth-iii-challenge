02b7b2fa903b7d5daafb193e2d470df8b22bb48e
nevets615 <surj316@yahoo.com>
2019-05-08 17:49:32 -0400

    server is running

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,17 +1,16 @@
-const jtw = require('jsonwebtoken')
-const secrets = require('../config/secrets.js')
+const jtw = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
 
 module.exports = (req, res, next) => {
- jwt.verify(token, secrets, (err, decodedToken) => {
-   if(err) {
-res.status(401).json({'no pass'})
-   } else {
+  jwt.verify(token, secrets, (err, decodedToken) => {
+    if (err) {
+      res.status(401).json({ you: "no pass" });
+    } else {
+      req.decodedToken = decodedToken;
+      next();
+    }
+  });
 
-    next()
-   }
- })
- 
- 
   const { username, password } = req.headers;
 
   if (username && password) {
@@ -21,13 +20,13 @@ res.status(401).json({'no pass'})
         if (user && bcrypt.compareSync(password, user.password)) {
           next();
         } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
+          res.status(401).json({ message: "Invalid Credentials" });
         }
       })
       .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
+        res.status(500).json({ message: "Ran into an unexpected error" });
       });
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({ message: "No credentials provided" });
   }
 };
---
+++ config/secrets.js
@@ -0,0 +1,8 @@
+module.exports = {
+    jwtSecret: process.env.JWT_SECRET || 'add this variable to your .env file',
+  
+    mantra: 'for many to many, third table',
+    otherMantra: 'one to many > Foreign Key',
+    yetAnotherMantra: 'where does the FK go... the many side',
+  };
+  
